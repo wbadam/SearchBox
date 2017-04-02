bb58741d5f064ce9e797027cfdbe63bf517af90c
adam <adam@vaadin.com>
2017-04-02 21:47:16 +0300

    Fix styling issues

--- src/test/java/org/vaadin/addons/searchbox/BasicSearchBoxUsageUI.java
+++ src/test/java/org/vaadin/addons/searchbox/BasicSearchBoxUsageUI.java
@@ -1,32 +1,58 @@
 package org.vaadin.addons.searchbox;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.vaadin.addonhelpers.AbstractTest;
 
-import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.VerticalLayout;
 
 public class BasicSearchBoxUsageUI extends AbstractTest {
 
     @Override
     public Component getTestComponent() {
-        SearchBox<String> searchBox = new SearchBox<>();
+        Layout layout = new VerticalLayout();
 
-        searchBox.setSuggestionGenerator((s, integer) -> Arrays
-                .asList(s + "1", s + "2", s + "3"));
+        SearchBox<String> searchBox1 = new SearchBox<>("Search", SearchBox.ButtonPosition.RIGHT);
+        searchBox1.setSuggestionGenerator(this::suggest);
+        layout.addComponent(searchBox1);
 
-        searchBox.setSearchButtonPosition(SearchBox.ButtonPosition.RIGHT);
-        searchBox.setSearchButtonIcon(VaadinIcons.SEARCH, "Search");
-        searchBox.setSearchButtonCaption(null);
+        SearchBox<String> searchBox2 = new SearchBox<>("Search", SearchBox.ButtonPosition.LEFT);
+        searchBox2.setSuggestionGenerator(this::suggest);
+        layout.addComponent(searchBox2);
 
-        searchBox.setPlaceholder("Search");
+        SearchBox<String> searchBox3 = new SearchBox<>();
+        searchBox3.setSearchButtonPosition(SearchBox.ButtonPosition.HIDDEN);
+        searchBox3.setSuggestionGenerator(this::suggest);
+        layout.addComponent(searchBox3);
 
-        searchBox.addSearchListener(event -> {
-            System.out.println(event.getSearchTerm());
-        });
+        SearchBox<String> searchBox4 = new SearchBox<>("Search", SearchBox.ButtonPosition.RIGHT);
+        searchBox4.setWidth("400px");
+        searchBox4.setSuggestionGenerator(this::suggest);
+        layout.addComponent(searchBox4);
 
-        return searchBox;
+        SearchBox<String> searchBox5 = new SearchBox<>("Search", SearchBox.ButtonPosition.LEFT);
+        searchBox5.setWidth("400px");
+        searchBox5.setSuggestionGenerator(this::suggest);
+        layout.addComponent(searchBox5);
+
+        SearchBox<String> searchBox6 = new SearchBox<>();
+        searchBox6.setSearchButtonPosition(SearchBox.ButtonPosition.HIDDEN);
+        searchBox6.setWidth("400px");
+        searchBox6.setSuggestionGenerator(this::suggest);
+        layout.addComponent(searchBox6);
+
+        return layout;
+    }
+
+    private List<String> suggest(String query, int limit) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < limit; i++) {
+            list.add(query + i);
+        }
+        return list;
     }
 
 }
